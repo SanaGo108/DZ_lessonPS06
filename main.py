4ebc737a9a7a2e5f0e32cb18fff9e07c05bf7f97
SanaGo108 <oksana135@mail.ru>
2024-06-27 00:03:09 +0300

    new1

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+url = "https://www.divan.ru/category/podvesnye-svetilniki"
+
+driver.get(url)
+time.sleep(3)
+
+vacancies = driver.find_elements(By.CLASS_NAME, 'VHx4T ZdH1P kQXIE')
+parsed_data = []
+for vacancy in vacancies:
+   try:
+       name = vacancy.find_element(By.CSS_SELECTOR, 'span itemprop="name"').text
+       price = vacancy.find_element(By.CSS_SELECTOR, 'meta itemprop="price"').text
+       link = vacancy.find_element(By.CSS_SELECTOR, 'a.itemprop="availability"').get_attribute('href')
+   except:
+       print("произошла ошибка при парсинге")
+       continue
+
+   parsed_data.append([name, price, link])
+
+driver.quit()
+
+with open("hh.csv", 'w',newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Название товара',  'цена', 'ссылка на товар'])
+    writer.writerows(parsed_data)
