5dbb9c46a3c6414d572aae7cac0ac24ecf340ea1
SanaGo108 <oksana135@mail.ru>
2024-07-01 15:08:08 +0300

    add new2

--- test1.py
+++ test1.py
@@ -10,13 +10,12 @@ url = "https://www.divan.ru/category/podvesnye-svetilniki"
 driver.get(url)
 time.sleep(3)
 
-# Изменил селектор для поиска элементов с товарами
+
 vacancies = driver.find_elements(By.CLASS_NAME, 'product-card')
 parsed_data = []
 
 for vacancy in vacancies:
     try:
-        # Поправил CSS селекторы для правильного извлечения данных
         name = vacancy.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
         price = vacancy.find_element(By.CSS_SELECTOR, 'meta[itemprop="price"]').get_attribute('content')
         link = vacancy.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
